e1762269b1dcba2c4e2f30e83e347469e50d51c8
Cesar Sanchez <cesar.s@vendorpanel.com>
2021-07-16 15:31:27 +1000

    initial commit

---
+++ mta/mta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
